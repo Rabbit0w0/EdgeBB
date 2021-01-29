68a498fdfa7cb654e6de9670fe53906428fa5fb9
Rabbit0w0 <401395069@qq.com>
2021-01-29 17:34:03 +0800

    fix

--- dashboard/login.php
+++ dashboard/login.php
@@ -40,14 +40,13 @@ $bodyClass = 'body-100';
 							<h3>Log in</h3>
 							<div class="input_outer">
 								<span class="u_user"></span>
-								<input name="logname" class="text" style="color: #FFFFFF !important" type="text" placeholder="请输入账户">
+								<input name="name" class="text" style="color: #FFFFFF !important" type="text" placeholder="请输入账户">
 							</div>
 							<div class="input_outer">
 								<span class="us_uer"></span>
-								<input name="logpass" class="text" style="color: #FFFFFF !important; position:absolute; z-index:100;"value="" type="password" placeholder="请输入密码">
+								<input name="password" class="text" style="color: #FFFFFF !important; position:absolute; z-index:100;"value="" type="password" placeholder="请输入密码">
 							</div>
 							<div class="mb2"><button class="act-but submit" href="javascript:;" style="color: #FFFFFF">登录</button></div>
-							<div class="blur"></div>
 						</form>
 					</div>
 				</div>
